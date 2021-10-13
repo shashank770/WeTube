78f7694ee915a46e8bd7e035678c4289ae476461
shashank770 <92398985+shashank770@users.noreply.github.com>
2021-10-13 20:50:29 +0530

    Add files via upload

---
+++ javaScript/app.js
@@ -0,0 +1,33 @@
+document.querySelector("")
+
+function clicked0 () {
+    location.href = "https://youtu.be/L-9s4nTLSdA"
+}
+
+function clicked1 () {
+    location.href = "https://youtu.be/8JT1JmTn3mo"
+}
+
+function clicked2 () {
+    location.href = "https://youtu.be/GLk7-imcjiI"
+}
+
+function clicked3 () {
+    location.href = "https://youtu.be/o5SQIECedTY"
+}
+
+function clicked4 () {
+    location.href = "https://youtu.be/kcW4ABcY3zI"
+}
+
+function clicked5 () {
+    location.href = "https://www.youtube.com/watch?v=kn271kr_ks0"
+}
+
+function clicked6 () {
+    location.href = "https://www.youtube.com/watch?v=AaZ_RSt0KP8&t=1047s"
+}
+
+function clicked7 () {
+    location.href = "https://www.youtube.com/watch?v=iCMGPRiDXQg&t=132s"
+}
